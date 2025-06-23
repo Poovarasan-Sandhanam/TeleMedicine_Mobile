ede83aa89c3097cfd35656f26be8d7dfcd2fe910
Poovarasan-Sandhanam <poovarasansandhanam@gmail.com>
2025-06-23 23:12:34 +0100

    Authentication Feature done using Formik + yup

--- src/styles/loginStyles.tsx
+++ src/styles/loginStyles.tsx
@@ -1,96 +1,127 @@
 import { StyleSheet, Platform } from 'react-native';
-import { COLORS } from '../constants/colors'; // Assuming you import your theme here
+import { COLORS } from '../constants/colors'; // Assuming you have a COLORS object defined
 
 export default StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.background,
   },
+
   scrollContainer: {
     flexGrow: 1,
     justifyContent: 'center',
     paddingHorizontal: 20,
-    paddingVertical: 30,
+    paddingVertical: 40,
   },
+
   title: {
     fontSize: 28,
-    fontWeight: 'bold',
-    marginBottom: 25,
+    fontWeight: '600',
+    marginBottom: 30,
     color: COLORS.primary,
     textAlign: 'center',
   },
+
   input: {
     height: 50,
     borderColor: COLORS.border,
     borderWidth: 1,
-    borderRadius: 8,
-    paddingHorizontal: 15,
+    borderRadius: 10,
+    paddingHorizontal: 16,
     fontSize: 16,
-    marginBottom: 12,
+    marginBottom: 8,
     color: COLORS.text,
     backgroundColor: COLORS.white,
-    shadowColor: COLORS.shadow,
-    shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.1,
-    shadowRadius: 2,
-    elevation: 2, // Android shadow
+    ...Platform.select({
+      ios: {
+        shadowColor: COLORS.shadow,
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 3,
+      },
+    }),
   },
+
   inputError: {
     borderColor: COLORS.error,
   },
+
   errorText: {
     color: COLORS.error,
-    fontSize: 12,
+    fontSize: 13,
     marginBottom: 10,
-    textAlign: 'center',
+    marginTop: 2,
+    paddingHorizontal: 4,
   },
+
   passwordContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     borderColor: COLORS.border,
     borderWidth: 1,
-    borderRadius: 8,
-    paddingHorizontal: 15,
-    marginBottom: 12,
+    borderRadius: 10,
+    paddingHorizontal: 16,
+    marginBottom: 8,
     backgroundColor: COLORS.white,
-    shadowColor: COLORS.shadow,
-    shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.1,
-    shadowRadius: 2,
-    elevation: 2,
+    ...Platform.select({
+      ios: {
+        shadowColor: COLORS.shadow,
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 3,
+      },
+    }),
   },
+
   passwordInput: {
     flex: 1,
     height: 50,
     fontSize: 16,
     color: COLORS.text,
   },
+
   button: {
     backgroundColor: COLORS.primary,
     paddingVertical: 15,
-    borderRadius: 8,
-    marginTop: 10,
-    marginBottom: 20,
+    borderRadius: 10,
+    marginTop: 20,
     alignItems: 'center',
     justifyContent: 'center',
-    shadowColor: COLORS.primary,
-    shadowOffset: { width: 0, height: 3 },
-    shadowOpacity: 0.3,
-    shadowRadius: 5,
-    elevation: 5,
+    ...Platform.select({
+      ios: {
+        shadowColor: COLORS.primary,
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.3,
+        shadowRadius: 5,
+      },
+      android: {
+        elevation: 4,
+      },
+    }),
   },
+
   buttonText: {
     color: COLORS.white,
-    fontWeight: 'bold',
-    fontSize: 18,
+    fontWeight: '600',
+    fontSize: 17,
   },
+
   contentText: {
     color: COLORS.primary,
     textAlign: 'center',
-    fontSize: 16,
+    fontSize: 15,
+    marginTop: 10,
     textDecorationLine: 'underline',
   },
+
   linkButton: {
     backgroundColor: 'transparent',
+    marginTop: 10,
   },
 });
